55b41e08e94bf857759c82442b302099ab2692f4
rmwa <rmwagner@gmx.de>
2015-08-30 14:26:11 +0000

    Added image upload for repipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,4 +7,18 @@ class Recipe < ActiveRecord::Base
   validates :description, presence: true, length: {minimum: 20, maximum: 500}
   validates :chef_id, presence: true
   
+  mount_uploader :picture, PictureUploader
+  
+  validate :picture_size
+  
+  
+  private
+  
+    def picture_size
+      if picture.size > 3.megabytes 
+        errors.add(:picture, "should be less than 3MB")
+      end
+    end
+  
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825084123) do
+ActiveRecord::Schema.define(version: 20150830124123) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150825084123) do
     t.integer  "chef_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "picture"
   end
 
 end
