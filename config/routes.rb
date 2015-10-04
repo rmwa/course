5be9881d8f27f8ed4b3fddc57d5c11e278d58def
rmwa <rmwagner@gmx.de>
2015-10-04 14:28:49 +0000

    Register chefs added

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,32 @@
 Rails.application.routes.draw do
   
+  get 'chefs/index'
+
+  get 'chefs/new'
+
+  get 'chefs/edit'
+
+  get 'chefs/destroy'
+
+  get 'chefs/create'
+
+  get 'chefs/show'
+
+  get 'chefs/update'
+
   root 'pages#home'
   get '/home', to: 'pages#home'
+  
   resources :recipes do
     member do
       post 'like'
     end
   end
   
-
+  resources :chefs, except: [:new]
+  get '/register', to: 'chefs#new'
+  
+  
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
