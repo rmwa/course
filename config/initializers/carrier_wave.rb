14c6df4a337258ce4f19e071058230281ab436ea
rmwa <rmwagner@gmx.de>
2015-10-25 16:58:10 +0000

    Added S3 v2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,6 +2,7 @@ if Rails.env.production?
   CarrierWave.configure do |c|
     c.fog_credentials = {
       provider: "AWS",
+      region: "eu-west-1",
       aws_access_key_id:     ENV["S3_ACCESS_KEY"],
       aws_secret_access_key: ENV["S3_SECRET_KEY"]
     }
